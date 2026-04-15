0386743795bad89681f2a044ebb92c5121e7ac06
Pontus Johansson <johanssonp998@gmail.com>
2026-04-15 10:17:00 +0200

    påbörjar server.js kopplar databasen, lägger till validering vid connection och startar server

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Database connection
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+// Validering för anslutning
+connection.connect((err) =>{
+    if (err) {
+        console.error("Connection to database failed: " + err);
+        return;
+    } 
+    console.log("Connected to MySQL database!");
+});
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Startar server
+app.listen(port, ()=> {
+    console.log(`Server is running on port: ${port}`);
+});
